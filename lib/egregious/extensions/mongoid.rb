25f64e2a1ca675cbc096d07bf2770c9fe04f1777
David Butler <dwbutler@ucla.edu>
2013-02-28 12:06:43 -0800

    Improvements to Mongoid error serialization

--- lib/egregious/extensions/mongoid.rb
+++ lib/egregious/extensions/mongoid.rb
@@ -1,13 +1,26 @@
 if defined?(Mongoid)
   # Customize the error information Egregious returns for MongoidErrors
+  module Mongoid
+    module Errors
+      class MongoidError
+        def to_xml
+          "<errors><error>#{HTMLEntities.new.encode(@summary || to_s)}</error><type>#{self.exception_type}</type></errors>"
+        end
 
-  Mongoid::Errors::MongoidError.class_eval do
-    def to_xml
-      "<errors><error>#{HTMLEntities.new.encode(self.document.errors.full_messages.join(', ').gsub)}</error><type>#{self.exception_type}</type></errors>"
-    end
+        def to_json
+          "{\"error\":#{ActiveSupport::JSON.encode(@summary || to_s)}, \"type\":\"#{self.exception_type}\"}"
+        end
+      end
+      
+      class Validations
+        def to_xml
+          "<errors><error>#{HTMLEntities.new.encode(self.document.errors.full_messages.join(', ').gsub)}</error><type>#{self.exception_type}</type></errors>"
+        end
 
-    def to_json
-      "{\"error\":#{ActiveSupport::JSON.encode(self.document.errors.full_messages.join(', ').gsub(/\r/, ' ').gsub(/\n/, ' ').squeeze(' '))}, \"type\":\"#{self.exception_type}\"}"
+        def to_json
+          "{\"error\":#{ActiveSupport::JSON.encode(self.document.errors.full_messages.join(', ').gsub(/\r/, ' ').gsub(/\n/, ' ').squeeze(' '))}, \"type\":\"#{self.exception_type}\"}"
+        end
+      end
     end
   end
 end
