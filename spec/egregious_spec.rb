0f84035cc073f4ef1292fc4172639482960b8259
rx <russell@voomify.com>
2015-10-22 23:43:06 -0400

    updated specs/gemfile to run latest

--- spec/egregious_spec.rb
+++ spec/egregious_spec.rb
@@ -23,7 +23,7 @@ describe Egregious do
     it "should return a stack trace" do
       begin
         raise Exception.new
-      rescue Exception=>exception
+      rescue Exception => exception
         clean_backtrace(exception).size.should be > 0
       end
     end
@@ -31,7 +31,7 @@ describe Egregious do
     it "should remove the beginning of the backtrace" do
       begin
         raise Exception.new
-      rescue Exception=>exception
+      rescue Exception => exception
         Rails.instance_eval do
           def self.root
             __FILE__
@@ -50,64 +50,64 @@ describe Egregious do
   describe 'exception_codes' do
 
     it "should return forbidden for SecurityError's'" do
-      exception_codes[SecurityError].should ==  Egregious.status_code(:forbidden)
+      exception_codes[SecurityError].should == Egregious.status_code(:forbidden)
     end
 
     if defined?(ActionController)
       it "should return expected errors for ActionController" do
-         exception_codes[AbstractController::ActionNotFound].should ==  Egregious.status_code(:bad_request)
-         exception_codes[ActionController::InvalidAuthenticityToken].should ==  Egregious.status_code(:bad_request)
-         exception_codes[ActionController::MethodNotAllowed].should ==  Egregious.status_code(:not_allowed)
-         exception_codes[ActionController::MissingFile].should ==  Egregious.status_code(:not_found)
-         exception_codes[ActionController::RoutingError].should ==  Egregious.status_code(:bad_request)
-         exception_codes[ActionController::UnknownController].should ==  Egregious.status_code(:bad_request)
-         exception_codes[ActionController::UnknownHttpMethod].should ==  Egregious.status_code(:not_allowed)
-         #exception_codes[ActionController::MissingTemplate].should ==  Egregious.status_code(:not_found)
+        exception_codes[AbstractController::ActionNotFound].should == Egregious.status_code(:bad_request)
+        exception_codes[ActionController::InvalidAuthenticityToken].should == Egregious.status_code(:bad_request)
+        exception_codes[ActionController::MethodNotAllowed].should == Egregious.status_code(:not_allowed)
+        exception_codes[ActionController::MissingFile].should == Egregious.status_code(:not_found)
+        exception_codes[ActionController::RoutingError].should == Egregious.status_code(:bad_request)
+        exception_codes[ActionController::UnknownController].should == Egregious.status_code(:bad_request)
+        exception_codes[ActionController::UnknownHttpMethod].should == Egregious.status_code(:not_allowed)
+        #exception_codes[ActionController::MissingTemplate].should ==  Egregious.status_code(:not_found)
       end
     end
 
     if defined?(ActiveModel)
       it "should return expected errors for ActiveModel" do
-        exception_codes[ActiveModel::MissingAttributeError].should ==  Egregious.status_code(:bad_request)
+        exception_codes[ActiveModel::MissingAttributeError].should == Egregious.status_code(:bad_request)
       end
     end
 
     if defined?(ActiveRecord)
       it "should return expected errors for ActiveRecord" do
-         exception_codes[ActiveRecord::AttributeAssignmentError].should ==  Egregious.status_code(:bad_request)
-         exception_codes[ActiveRecord::MultiparameterAssignmentErrors].should ==  Egregious.status_code(:bad_request)
-         exception_codes[ActiveRecord::ReadOnlyAssociation].should ==  Egregious.status_code(:forbidden)
-         exception_codes[ActiveRecord::ReadOnlyRecord].should ==  Egregious.status_code(:forbidden)
-         exception_codes[ActiveRecord::RecordInvalid].should ==  Egregious.status_code(:bad_request)
-         exception_codes[ActiveRecord::RecordNotFound].should ==  Egregious.status_code(:not_found)
-         exception_codes[ActiveRecord::UnknownAttributeError].should ==  Egregious.status_code(:bad_request)
+        exception_codes[ActiveRecord::AttributeAssignmentError].should == Egregious.status_code(:bad_request)
+        exception_codes[ActiveRecord::MultiparameterAssignmentErrors].should == Egregious.status_code(:bad_request)
+        exception_codes[ActiveRecord::ReadOnlyAssociation].should == Egregious.status_code(:forbidden)
+        exception_codes[ActiveRecord::ReadOnlyRecord].should == Egregious.status_code(:forbidden)
+        exception_codes[ActiveRecord::RecordInvalid].should == Egregious.status_code(:bad_request)
+        exception_codes[ActiveRecord::RecordNotFound].should == Egregious.status_code(:not_found)
+        exception_codes[ActiveRecord::UnknownAttributeError].should == Egregious.status_code(:bad_request)
       end
     end
 
     if defined?(Warden)
       it "should return expected errors for Warden" do
-        exception_codes[Warden::NotAuthenticated].should ==  Egregious.status_code(:unauthorized)
+        exception_codes[Warden::NotAuthenticated].should == Egregious.status_code(:unauthorized)
       end
     end
 
     if defined?(CanCan)
       it "should return expected errors for CanCan" do
         # technically this should be forbidden, but for some reason cancan returns AccessDenied when you are not logged in
-        exception_codes[CanCan::AccessDenied].should ==  Egregious.status_code(:unauthorized)
-        exception_codes[CanCan::AuthorizationNotPerformed].should ==  Egregious.status_code(:unauthorized)
+        exception_codes[CanCan::AccessDenied].should == Egregious.status_code(:unauthorized)
+        exception_codes[CanCan::AuthorizationNotPerformed].should == Egregious.status_code(:unauthorized)
       end
     end
-    
+
     if defined?(Mongoid)
       it "should return expected errors for Mongoid" do
         exception_codes[Mongoid::Errors::InvalidFind].should == Egregious.status_code(:bad_request)
         exception_codes[Mongoid::Errors::DocumentNotFound].should == Egregious.status_code(:not_found)
         exception_codes[Mongoid::Errors::Validations].should == Egregious.status_code(:unprocessable_entity)
       end
-      
+
       if Mongoid::VERSION > '3'
         it "should return expected errors for Mongoid 3+" do
-          exception_codes[Mongoid::Errors::ReadonlyAttribute].should ==  Egregious.status_code(:forbidden)
+          exception_codes[Mongoid::Errors::ReadonlyAttribute].should == Egregious.status_code(:forbidden)
           exception_codes[Mongoid::Errors::UnknownAttribute].should == Egregious.status_code(:bad_request)
         end
       end
@@ -115,13 +115,33 @@ describe Egregious do
   end
 
   describe "status_code_for_exception" do
-    it 'should return 500 for non-mapped exceptions'do
+    it 'should return 500 for non-mapped exceptions' do
       exception_codes[Exception].should == nil
       status_code_for_exception(Exception.new).should=='500'
     end
     it 'should allow configuration of exception codes' do
       Egregious.exception_codes.merge!({NameError => "999"})
       status_code_for_exception(NameError.new).should=="999"
+    end
+  end
+
+  describe "exception with http_status method" do
+    class PartyLikeIts < StandardError
+      def http_status
+        1999
+      end
+    end
+
+    it 'should honor if not in exception code map' do
+      exception_codes[PartyLikeIts].should == nil
+      status_code_for_exception(PartyLikeIts.new).should=='1999'
+    end
+
+    it 'should be overridden by exception code map' do
+      exception_codes.merge! ({PartyLikeIts: status_code(:bad_request)})
+      exception_codes[PartyLikeIts].should == nil
+      status_code_for_exception(PartyLikeIts.new).should==status_code(:bad_request)
+
     end
   end
 
